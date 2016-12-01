1462f922f6cd0990be2ed3be6fd02436eab8e07a
AGARWAL <akshatagarwal@northwesternmutual.com>
2016-12-01 14:05:47 -0600

    added hellojava.java

---
+++ hellojava.java
@@ -0,0 +1,11 @@
+public class hellojava{
+	
+	public static void main (String [] args){
+		System.out.println("hello");
+		for (int i=1; i<=10; i++){
+			System.out.println("hello world"+i);
+		}
+		
+	}
+	
+}
